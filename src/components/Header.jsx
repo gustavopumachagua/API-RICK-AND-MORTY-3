658f24e4733c53301fa4bb99cb48fe751b6ccade
puma <gustavopumachagua.com>
2022-10-13 22:28:35 -0500

    final2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import logo from "../assets/fondo.png";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 function Header() {
   return (
     <header className="bg-bodyLight dark:bg-bodyDark">
       <div className="bg-auto bg-center px-8 md:px-20 lg:px-64">
-        <img src={logo} alt="logo" />
+        <LazyLoadImage
+          width="100%"
+          min-height="144"
+          effect="blur"
+          src={logo}
+          alt="logo"
+        />
       </div>
     </header>
   );
